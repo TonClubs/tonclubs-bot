61eea9dd599696087ba0dce575339f4ab9f3c44c
Uğur Eren <contact@ugureren.net>
2023-03-17 16:25:45 +0300

    Set default admin rights

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,23 @@ import {Bot, BotInfo, Debug} from './Services';
 
   if (!ok) throw new Error('Could not set commands');
 
+  await Bot.setMyDefaultAdministratorRights({
+    rights: {
+      is_anonymous: false,
+      can_manage_chat: true,
+      can_delete_messages: true,
+      can_manage_video_chats: false,
+      can_restrict_members: true,
+      can_promote_members: true,
+      can_change_info: false,
+      can_invite_users: true,
+      can_post_messages: true,
+      can_edit_messages: true,
+      can_pin_messages: false,
+      can_manage_topics: false,
+    },
+  });
+
   const me = await Bot.getMe();
 
   BotInfo.setBotInfo(me);
